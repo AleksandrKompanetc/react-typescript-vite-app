adcf447d688911bc504c4dec080629360db51c08
AleksandrKompanetc <moneymacker7@gmail.com>
2025-01-29 23:34:19 +0200

    added posts request

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 
 interface Post {
@@ -8,11 +8,26 @@ interface Post {
 }
 
 const App: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([])
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(data => setPosts(data))
+    .catch(error => console.log('Error fetching posts:', error))
+  }, []);
 
   return (
     <div className='container'>
       <h1>Posts</h1>
-      <div className='posts-wrapper'></div>
+      <div className='posts-wrapper'>
+        {posts.map(post => (
+          <div key={post.id} className='post-card'>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
