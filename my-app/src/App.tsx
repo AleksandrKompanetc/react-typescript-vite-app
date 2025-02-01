fd883732e950e7555e0d1cbacfca239b726b658b
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-01 23:55:26 +0200

    added changes for pagination

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -14,6 +14,7 @@ const App: React.FC = () => {
   const [newTitle, setNewTitle] = useState<string>('')
   const [newBody, setNewBody] = useState<string>('')
   const [currentPage, setCurrentPage] = useState<number>(1)
+  const postPerPage = 9
 
   const fetchPosts = () => {
     setLoading(true)
