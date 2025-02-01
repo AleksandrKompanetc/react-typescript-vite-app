39f60e96933bccf8acb7dd8f9ef15ee13cf6a0d6
AleksandrKompanetc <moneymacker7@gmail.com>
2025-02-01 23:17:13 +0200

    added a limit on the number of posts displayed on first load

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -19,7 +19,7 @@ const App: React.FC = () => {
     fetch('https://jsonplaceholder.typicode.com/posts')
     .then(response => response.json())
     .then(data => {
-      setPosts(data)
+      setPosts(data.slice(0, 9))
       setLoading(false)
     })
     .catch(error => {
